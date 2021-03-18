91370d4e4fe130c3676b86d13fd8ebd0f2e7931b
pravallika <pravallikaammineni@github.com>
2021-03-18 14:47:57 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.css'
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-center">
+    <p>This is my heading</p>
+    </div>
+  );
+}
+
+export default App;
